6339fdfeaa03300651682736ba5436ae69a2ff39
Vladimir Zaets <vzaets@adobe.com>
2022-08-11 15:01:10 -0500

    Add env file support

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,21 @@
 "use strict";
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+require('dotenv').config();
 const axios = require('axios').default;
 class ConnectorRegistryStorage {
-    constructor() {
-        this.metadataPath = "https://raw.githubusercontent.com/devx-services/connector-registry/dev/connectors-metadata.json";
-        this.archivePath = "https://raw.githubusercontent.com/devx-services/connector-registry/dev/archive";
-    }
     async getList() {
         try {
-            const response = await axios.get(this.metadataPath);
+            const response = await axios.get(process.env.METADATA_SOURCE_PATH);
             return response.data;
         }
         catch (error) {
@@ -24,7 +32,7 @@ class ConnectorRegistryStorage {
         }
     }
     generateArchivePath(name, version) {
-        return `${this.archivePath}/${version}-${name}.json`;
+        return `${process.env.ARCHIVE_SOURCE_PATH}/${version}-${name}.json`;
     }
 }
 module.exports = ConnectorRegistryStorage;
